50239a809bd765140661bb06c47260ea6992d0da
Andrea Grillo <andrea.grillo96@live.com>
2022-09-01 12:05:04 +0200

    feat: Add new instance in separate state

---
+++ aws-ec2-apache/new_state/main.tf
@@ -0,0 +1,57 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 3.24.1"
+    }
+  }
+  required_version = ">= 1.1.5"
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+data "aws_security_group" "sg_8080" {
+  filter {
+    name = "group-name"
+    values = ["*sg-8080*"]
+  }
+}
+
+resource "aws_instance" "example_new" {
+  ami                    = data.aws_ami.ubuntu.id
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [data.aws_security_group.sg_8080.id]
+  user_data              = <<-EOF
+              #!/bin/bash
+              apt-get update
+              apt-get install -y apache2
+              sed -i -e 's/80/8080/' /etc/apache2/ports.conf
+              echo "Hello World" > /var/www/html/index.html
+              systemctl restart apache2
+              EOF
+  tags = {
+    Name = "terraform-learn-state-ec2"
+  }
+}
+
+output "public_ip" {
+    value = aws_instance.example_new.public_ip
+}
