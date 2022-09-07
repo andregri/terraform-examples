deadc28550f71635f89ba550b37158c5443da7c5
Andrea Grillo <andrea.grillo96@live.com>
2022-09-07 11:49:12 +0200

    update rds instance

--- aws-vault-jenkins/app/database.tf
+++ aws-vault-jenkins/app/database.tf
@@ -1,16 +1,16 @@
-resource "aws_db_instance" "app" {
+resource "aws_db_instance" "appdb" {
   allocated_storage    = 5
   engine               = "postgres"
   instance_class       = "db.t3.micro"
-  name                 = "mydb"
+  db_name              = "mydb"
   username             = var.username
   password             = var.password
   skip_final_snapshot  = true
-  db_subnet_group_name = aws_db_subnet_group.default.id
+  db_subnet_group_name = aws_db_subnet_group.devgroup.id
   vpc_security_group_ids = [ aws_security_group.allow_postgres.id ]
 }
 
-resource "aws_db_subnet_group" "default" {
+resource "aws_db_subnet_group" "devgroup" {
   name       = "main"
   subnet_ids = module.aws_vpc.public_subnets
 
