c56c812f1376a318293dacb6b4d93f7d28593a1c
Andrea Grillo <andrea.grillo96@live.com>
2022-09-13 15:53:06 +0200

    Use withVault to get static and dynamic secrets

--- aws-vault-jenkins/Jenkinsfile.groovy
+++ aws-vault-jenkins/Jenkinsfile.groovy
@@ -3,7 +3,7 @@ import com.cloudbees.plugins.credentials.CredentialsScope
 import com.datapipe.jenkins.vault.credentials.VaultTokenCredential
 
 def vault_cred_id = 'vault-jenkins-auth'
-def vault_addr = 'http://44.197.182.74:8200'
+def vault_addr = 'http://54.221.172.71:8200'
 
 pipeline {
     agent any
@@ -89,7 +89,12 @@ pipeline {
             }
         }
 
-        stage('Test') {
+        stage('Build app infrastructure with Terraform') {
+            environment {
+                TF_VAR_aws_region = 'us-east-1'
+                TF_VAR_key_name = 'vault-kp'
+                TF_VAR_tpl_vault_server_addr = "${vault_addr}"
+            }
             steps {
                 script {
                     VaultTokenCredential pipelineCredential = new VaultTokenCredential(
@@ -102,95 +107,23 @@ pipeline {
                             vaultCredential: pipelineCredential]
                     def secrets = [
                         [path: 'db-creds/db', engineVersion: 1, secretValues: [
-                            [envVar: 'testing', vaultKey: 'user'],
-                            [envVar: 'testing_again', vaultKey: 'password']]],
+                            [envVar: 'TF_VAR_username', vaultKey: 'user'],
+                            [envVar: 'TF_VAR_password', vaultKey: 'password']]],
                         [path: 'aws/creds/pipeline-role', engineVersion: 1, secretValues: [
-                            [envVar: 'testing1', vaultKey: 'access_key'],
-                            [envVar: 'testing_again1', vaultKey: 'secret_key']]]
+                            [envVar: 'AWS_ACCESS_KEY_ID', vaultKey: 'access_key'],
+                            [envVar: 'AWS_SECRET_ACCESS_KEY', vaultKey: 'secret_key']]]
                     ]
                     withVault([configuration: configuration, vaultSecrets: secrets]) {
-                        sh '''
-                        echo $testing1 > /tmp/creds
-                        echo $testing_again1 >> /tmp/creds
-                        cat /tmp/creds
-                        '''
-                    }
-                }
-            }
-        }
-
-        stage('Build app infrastructure with Terraform') {
-            environment {
-                TF_VAR_aws_region = 'us-east-1'
-                TF_VAR_key_name = 'vault-kp'
-                TF_VAR_tpl_vault_server_addr = "${vault_addr}"
-            }
-            steps {
-                script {
-                    // Read credentials to initialize db
-                    def credentials_db = ""
-                    credentials_db = sh(
-                        returnStdout: true,
-                        script: """
-                            set +x
-                            curl --silent -H "X-Vault-Request: true" -H "X-Vault-Token: ${env.PIPELINE_VAULT_TOKEN}" ${vault_addr}/v1/db-creds/db
-                        """
-                    )
-
-                    env.TF_VAR_username = sh(
-                        returnStdout: true,
-                        script: """
-                            set +x
-                            echo '${credentials_db}' | jq -r '.data.user'
-                        """
-                    ).trim()
-
-                    env.TF_VAR_password = sh(
-                        returnStdout: true,
-                        script: """
-                            set +x
-                            echo '${credentials_db}' | jq -r '.data.password'
-                        """
-                    ).trim()
-
-                    // Get temporary AWS credentials
-                    credentials_aws = sh(
-                        returnStdout: true,
-                        script: """
-                            set +x
-                            curl --silent -H "X-Vault-Request: true" -H "X-Vault-Token: ${PIPELINE_VAULT_TOKEN}" ${vault_addr}/v1/aws/creds/pipeline-role
-                        """
-                    )
-
-                    env.AWS_ACCESS_KEY_ID = sh(
-                        returnStdout: true,
-                        script: """
-                            set +x
-                            echo '${credentials_aws}' | jq -r '.data.access_key'
-                        """
-                    ).trim()
-
-                    env.AWS_SECRET_ACCESS_KEY = sh(
-                        returnStdout: true,
-                        script: """
-                            set +x 
-                            echo '${credentials_aws}' | jq -r '.data.secret_key'
-                        """
-                    ).trim()
-
-                    /*git branch: 'main', url: 'https://github.com/andregri/terraform-examples.git'
+                        
+                        git branch: 'main', url: 'https://github.com/andregri/terraform-examples.git'
                     
-                    sh("""
-                        cd "${WORKSPACE}/aws-vault-jenkins/app"
-                        terraform init -no-color
-                        terraform apply -auto-approve -no-color
-                        terraform destroy -auto-approve -no-color
-                    """)*/
-                }
-            }
-            post {
-                failure {
-                    sh 'terraform destroy -auto-approve -no-color'
+                        sh("""
+                            cd "${WORKSPACE}/aws-vault-jenkins/app"
+                            terraform init -no-color
+                            terraform apply -auto-approve -no-color
+                            terraform destroy -auto-approve -no-color
+                        """)
+                    }
                 }
             }
         }
